4305aa6102910388893897cc450b2775d1c171ff
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-08-02 09:02:40 -0600

    Added in card for my picture

--- personal-website/src/components/pages/AboutMe.js
+++ personal-website/src/components/pages/AboutMe.js
@@ -1,20 +1,20 @@
 import React from "react";
 import '../../App.css'
 import image5 from '../../images/img-5.jpg'
-import '../../../src/components/pages/Cards.css'
+import './AboutMe.css'
 export default function AboutMe() {
     return (<> 
     <h1 className='aboutme'> ABOUT ME </h1> 
-    <div className=".image-container"> 
-            <figure className='cards__item__pic-wrap' data-category="My Story">
-                
-                <img src={image5} alt="Travel Image"></img>
-
+    <li className='cards__item'> 
+          <div className="cards__item__link">
+            <figure className='cards__item__pic-wrap'>
+              <img src={image5} alt="Travel Image" className='cards__item__img'></img>
             </figure>
             <div className='cards__item__info'>
-              <h5 className='cards_item_text' > A Nice pic of Me </h5>
+              <h5 className='cards_item_text' > A nice Picture of Me </h5>
             </div>
-    </div>
+          </div>
+        </li>
 
     </>)
 }
