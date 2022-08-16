cc62ae7ccd704cdcd40c17632ae20420697a5cca
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-08-16 08:58:24 -0600

    added in photo of myself to aboutme page

--- personal-website/src/components/pages/AboutMe.js
+++ personal-website/src/components/pages/AboutMe.js
@@ -1,17 +1,15 @@
 import React from "react";
 import '../../App.css'
-import image5 from '../../images/img-5.jpg'
+import image5 from '../../images/myWebsitePic.jpg'
 import './AboutMe.css'
 export default function AboutMe() {
     return (<> 
     <h1 className='aboutme'> ABOUT ME </h1> 
-    <li className='cards__item'> 
-          <div className="cards__item__link">
-            <figure className='cards__item__pic-wrap'>
-              <img src={image5} alt="Travel Image" className='cards__item__img'></img>
-            </figure>
-          </div>
-        </li>
+      <div> 
+        
+        <img src={image5} alt="Travel Image" className='myPicture'></img>
+       
+      </div>     
 
     </>)
 }
