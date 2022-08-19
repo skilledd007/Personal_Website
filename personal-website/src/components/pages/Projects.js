a2c45bdb8c734d7d8977f9ba3b96ed89b0439f25
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-08-19 12:21:33 -0600

    Cards for Projects page complete

--- personal-website/src/components/pages/Projects.js
+++ personal-website/src/components/pages/Projects.js
@@ -1,14 +1,23 @@
 import React from "react";
 import '../../App.css'
 import './Projects.css'
-import batteryAdvancer from '../../images/BatteryAdvancer.jpeg'
+import './BatteryAdvancer.jpg'
 export default function Projects() {
     return (<> 
     <h1 className='page-name-projects'> PROJECTS </h1> 
       <div className='cardy'> 
-        <div className='cardy-element' >  <p> The Battery Advancer  </p> </div>
-        <div className='cardy-element' >  <p> The Battery Advancer Image  </p> </div>
-      </div>
-      
+          <div className='text-div' > The Battery Advancer </div>
+         <img src={require('./BatteryAdvancer.jpg')} className='image-class'/> 
+      </div>  
+      <div className="card-spacer"/> 
+      <div className='cardy'> 
+          <div className='text-div' > nosTicket </div>
+         <img src={'https://media1.s-nbcnews.com/i/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.jpg'} className='image-class'/> 
+      </div> 
+      <div className="card-spacer" />
+      <div className='cardy'> 
+          <div className='text-div' > The Battery Advancer </div>
+         <img src={'https://media1.s-nbcnews.com/i/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.jpg'} className='image-class'/> 
+      </div>            
     </>)
 }
