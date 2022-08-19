03c8277f5f5ca188165bbc25e17ee8b612ff9d65
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-08-19 11:10:45 -0600

    Got card for projects page working

--- personal-website/src/components/pages/Projects.js
+++ personal-website/src/components/pages/Projects.js
@@ -1,9 +1,14 @@
 import React from "react";
 import '../../App.css'
-
+import './Projects.css'
+import batteryAdvancer from '../../images/BatteryAdvancer.jpeg'
 export default function Projects() {
     return (<> 
     <h1 className='page-name-projects'> PROJECTS </h1> 
+      <div className='cardy'> 
+        <div className='cardy-element' >  <p> The Battery Advancer  </p> </div>
+        <div className='cardy-element' >  <p> The Battery Advancer Image  </p> </div>
+      </div>
       
     </>)
 }
