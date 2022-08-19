adc51d7258a1a260caec8e2ccc4f2ab8987a282a
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-08-19 12:38:50 -0600

    Added picture for nosTicket app

--- personal-website/src/components/pages/Projects.js
+++ personal-website/src/components/pages/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../../App.css'
 import './Projects.css'
 import './BatteryAdvancer.jpg'
+import './nosTicket.jpg'
 export default function Projects() {
     return (<> 
     <h1 className='page-name-projects'> PROJECTS </h1> 
@@ -12,7 +13,7 @@ export default function Projects() {
       <div className="card-spacer"/> 
       <div className='cardy'> 
           <div className='text-div' > nosTicket </div>
-         <img src={'https://media1.s-nbcnews.com/i/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.jpg'} className='image-class'/> 
+         <img src={require('./nosTicket.jpg')} className='image-class'/> 
       </div> 
       <div className="card-spacer" />
       <div className='cardy'> 
